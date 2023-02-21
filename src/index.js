5b2a7e968826a1602265c400bd71d4d07a451af7
Viktoriya <kruckovaviktoria421@gmail.com>
2023-02-22 00:53:33 +0300

    init: create a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const openingBrackets = new Set();
+  const closingBrackets = new Set();
+  const matchingBrackets = {};
+  const stack = [];
+  const stackSecond = [];
+  for (let [opening, closing] of bracketsConfig) {
+    openingBrackets.add(opening);
+    closingBrackets.add(closing);
+    matchingBrackets[opening] = closing;
+  }
+  for (let char of str) {
+    if (openingBrackets.has(char) && (closingBrackets.has(char))) {
+      stackSecond.push(char)
+    } else if (openingBrackets.has(char)) {
+      stack.push(char);
+    } else if (closingBrackets.has(char)) {
+      const lastOpening = stack.pop();
+      if (matchingBrackets[lastOpening] !== char) {
+        return false;
+      }
+    }
+  }
+  if (stack.length == 0 && stackSecond.length % 2 === 0) {
+    return true
+  } else return false
+    
+  }
+
